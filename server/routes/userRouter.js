090eb77a435aeabe593bee4ea804e623e1863184
Sergey Kruptsov <serega.by@inbox.ru>
2022-08-19 13:52:06 +0300

    feat: create router application

---
+++ server/routes/userRouter.js
@@ -0,0 +1,12 @@
+const Router = require('express')
+const userController = require('../controllers/userController')
+const router = new Router()
+const UserController = require('../controllers/userController')
+
+
+router.post('/registration', userController.registration)
+router.get('/login', userController.login)
+router.get('/auth', userController.check)
+
+
+module.exports = router
